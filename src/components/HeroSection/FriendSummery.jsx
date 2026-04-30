cae80ef2195acfcf0af8fb6f4088c8a5e2a36397
Sagor220 <sagormahedi7@gmail.com>
2026-05-01 04:44:15 +0600

    readme.md added along with responsiveness also be attached

--- src/components/HeroSection/FriendSummery.jsx
+++ src/components/HeroSection/FriendSummery.jsx
@@ -1,6 +1,6 @@
 const FriendSummery = () => {
   return (
-    <div className="max-w-275 mx-auto grid grid-cols-4 gap-4 p-4">
+    <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 max-w-275 mx-auto">
       <div className="shadow text-center px-7 py-5 rounded-lg">
         <p className="text-2xl font-semibold text-[#244d3f] ">30</p>
         <p className="text-sm text-gray-700">Total Friends</p>
--- src/components/HeroSection/FriendsSection.jsx
+++ src/components/HeroSection/FriendsSection.jsx
@@ -7,7 +7,7 @@ const FriendsSection = ({ frndsData }) => {
   return (
     <div className="max-w-275 mx-auto my-10 mb-20">
       <h2 className="text-2xl font-bold">Your Friends</h2>
-      <div className="grid grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 max-w-275 mx-auto">
         {friends.map((friend) => (
           <FriendCard key={friend.id} friend={friend} />
         ))}
